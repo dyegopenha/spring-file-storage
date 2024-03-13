b3b4bd46ee8dc6cc917f4588f5113b5a53a0f571
Dyego Penha <dyegopenha@hotmail.com>
2024-03-13 09:25:21 -0300

    first commit

---
+++ src/main/java/com/example/springfilestorage/SpringFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springfilestorage/SpringFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
